026742c58a27fcebece62937c880f770c3f2fd40
03 <52514670+Luffy03@users.noreply.github.com>
2024-12-31 12:21:02 +0800

    Update Free_train.sh

--- FreeTumor-Abdomen/Free_train.sh
+++ FreeTumor-Abdomen/Free_train.sh
@@ -2,15 +2,20 @@ now=$(date +"%Y%m%d_%H%M%S")
 logdir=runs/logs_free_lits
 data=lits
 task=freesyn
+max_epochs=20
+val_every=1
 baseline_seg_dir=./baseline/model_baseline_segmentor.pt
 TGAN_checkpoint=./baseline/TGAN.pt
 
 mkdir -p $logdir
 
 export CUDA_VISIBLE_DEVICES=0
-torchrun --master_port=25023 Free_train.py \
+torchrun --master_port=25024 Free_train.py \
     --data $data \
     --task $task \
+    --max_epochs $max_epochs \
+    --val_every $val_every \
+    --use_ssl_pretrained \
     --baseline_seg_dir $baseline_seg_dir \
     --TGAN_checkpoint $TGAN_checkpoint \
     --logdir $logdir | tee $logdir/$now.txt
